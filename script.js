08f8698451a167fc70c10a231d93d891f415c297
Leigh Jeffery <leighjeffery@yahoo.ca>
2024-05-28 12:23:25 -0600

    New footer
    Add new page for footer
    add new js to insert footer
    built footer

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ fetch("nav.html")
   .then((data) => {
     document.getElementById("navbar").innerHTML = data;
   });
+
+// Fetch and insert footer
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+// keeps footer copyright up to date //
+document.addEventListener("DOMContentLoaded", function () {
+  var currentYear = new Date().getFullYear();
+  document.getElementById("current-year").textContent = currentYear;
+});
