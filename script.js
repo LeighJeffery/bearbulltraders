334894c7af95a929b81b41fd6d8de8f3513309c1
Leigh Jeffery <leighjeffery@yahoo.ca>
2024-06-13 15:50:03 -0600

    add back top buttons

--- script.js
+++ script.js
@@ -150,3 +150,26 @@ function initializeAccordion() {
 
 // Run the accordion initialization on initial load
 document.addEventListener("DOMContentLoaded", initializeAccordion);
+
+// Back to top button
+// Get the button
+let mybutton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
