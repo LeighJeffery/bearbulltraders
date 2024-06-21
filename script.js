6a638ca3435f6502b9a503dd05fcf788fe363989
Leigh Jeffery <leighjeffery@yahoo.ca>
2024-06-21 16:06:39 -0600

    fixed back to top button

--- script.js
+++ script.js
@@ -150,15 +150,6 @@ function initializeAccordion() {
 // Run the accordion initialization on initial load
 document.addEventListener("DOMContentLoaded", initializeAccordion);
 
-// Back to top button
-// Get the button
-let mybutton = document.getElementById("backToTopBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
 // Cookie POP-up and include
 document.addEventListener("DOMContentLoaded", function () {
   // Fetch and insert Cookie-pop HTML
@@ -232,4 +223,26 @@ document.addEventListener("DOMContentLoaded", function () {
       lazyBackground.classList.add("loaded");
     });
   }
+});
+
+// Get the button
+let mybutton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
